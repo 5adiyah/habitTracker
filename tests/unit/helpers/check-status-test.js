4c81ad38d7256f683cb8e46a4db4e0c221c7d468
5adiyah <sadiali@pdx.edu>
2016-06-16 00:11:31 -0700

    Add done/undone coloring feature

---
+++ tests/unit/helpers/check-status-test.js
@@ -0,0 +1,10 @@
+import { checkStatus } from 'code100days/helpers/check-status';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | check status');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = checkStatus([42]);
+  assert.ok(result);
+});
